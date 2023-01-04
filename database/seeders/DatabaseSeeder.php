8ad7e494fa7851b66f86bb817c3f711a960ad1b5
anugyal6 <sanugya20@tbc.edu.np>
2023-01-04 16:38:37 +0545

    third

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,13 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
+     *change bhaxa
      * @return void
      */
     public function run()
@@ -20,5 +21,11 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('users')->insert([
+            'email' => 'admin@admin.com',
+            'password'=>Hash::make('password'),
+            'name'=> 'admin',
+            'is_admin'=>1
+        ]);
     }
 }
